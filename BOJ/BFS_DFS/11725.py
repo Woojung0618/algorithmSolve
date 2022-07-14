9a0b2cadf7dfcf28022107db6e7e6dbd442d7cdd
Woojung0618 <woojung0618@gmail.com>
2022-07-14 23:49:13 +0900

    [BOJ] 실버 2 BFS (try 1 success)

---
+++ BOJ/BFS_DFS/11725.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+n = int(input())
+graph = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
+parent = [0] * (n+1)
+for _ in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+# print(graph)
+
+def bfs(s):
+    queue = deque([s])
+    visit[s] = 1
+    while queue:
+        x = queue.popleft()
+        for i in graph[x]:
+            if visit[i] == 0:
+                queue.append(i)
+                visit[i] = 1
+                parent[i] = x
+    return parent
+answer = bfs(1)
+print(answer)
+for a in range(2, len(answer)):
+    print(answer[a])
