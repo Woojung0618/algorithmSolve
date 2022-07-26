521c9fe11b9fc5baae5b4b22a6145d680a1e44fd
Woojung0618 <woojung0618@gmail.com>
2022-07-26 13:12:13 +0900

    [ch6. 정렬] 선택정렬

---
+++ 이것이코딩테스트다/정렬/선택정렬.py
@@ -0,0 +1,12 @@
+'''
+[선택정렬]
+정렬되지 않은 데이터 중 가장 작은 값을 '선택'하여 '맨앞'으로 보낸다.
+'''
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+for i in range(len(array)):
+    min_index = i  # min_index: 최솟값을 찾기 위한 인덱스
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]  # 최솟값과 맨앞을 swap
+print(array)
